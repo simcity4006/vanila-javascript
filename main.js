52041541e5264aed608c5ee2fd46c4de03856ae2
simcity4006 <114875423+simcity4006@users.noreply.github.com>
2023-02-09 23:54:01 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,471 @@
+// var foysal=true;
+// console.log(typeof foysal)    variable and var typeof niye kaj kora hoye se;
+// var kah=" FDKLFDJFKDFDFKJDKJFDKJFDKFDF";
+// console.log(kah.toLocaleLowerCase());### akhane upper and lower case niye kaj kora hoyese;
+// var familyName="foysal, forhad, faruk, majeda, maksuda, mahmuda";
+// console.log(familyName.split("foysal"))## akhaner onek gola name theke kono name ber korar jonn
+// var familyName="foysal, forhad, faruk, majeda, maksuda, mahmuda";
+// console.log(familyName.indexOf("foysal"))### konda kot tom ase ta janar jonn 
+// var num1="336";
+// num1 =parseFloat(num1) ### str theke number a ana jonn 
+// console.log(typeof num1);
+                                                // var num1 =525.255;
+                                                // var  num2 =545;
+                                                // num1 =parseInt(num1) number theke int
+                                                // var mot =num1+ num2;
+                                                // console.log(mot);
+// var num1 =215;
+// var num2=6545;
+// num1=""+num1;    number theke int
+// var total=num1+num2;
+// console.log(total);
+
+// var num1=-85;
+// var dklfj=Math.abs(num1); #$# aita odbe na---------
+// console.log(dklfj);
+
+// var num1 =5.5;
+// var mot =Math.round(num1); ### 0.5 hole opore and 0.2 hole niche
+// console.log(mot);
+
+// var  num1=0.222;
+// var mot =Math.ceil(num1);   up up up up up up 
+// console.log(mot);
+
+// var  num1=0.222;
+// var mot =Math.floor(num1); daon daon daon daon daon daon daon daon daon daon 
+// console.log(mot);
+
+// var moja =Math.random() *10;
+// console.log(moja);          
+
+
+
+// var a=5;
+// // var b=6;
+
+// if(a>33){
+//     console.log("d")
+// }
+
+// else if(a>40){
+//    console.log("c")
+// }
+// else if(a>50){
+//    console.log("b")
+// }                                                        #if else ar jonn enogh
+// else if(a>60){
+//    console.log("a-")
+// }
+// else if(a>70){
+//    console.log("a")
+// }
+// else if(a>80){
+//    console.log("a+")
+// }
+// else if(a>90){
+//    console.log("A+")
+// }
+// else {
+//     console.log("tumi dim paisu")
+// }
+
+// var mot=545;
+// switch(mot){
+//     case 10:
+//     console.log("hello word");
+//     break;
+//     case 5:
+//     console.log("i am a 5")
+//     break;                           switch
+//     case 15:
+//     console.log("i am a 15")
+//     break;
+//     default:
+//     console.log("ami tumake ar pelam na")
+
+// }
+
+// var myDate= new Date();
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleDateString());
+
+
+// var foysalName=["foysal", "ahmed", "sorker", "khan", "simcity"];
+// foysalName[2]="hasina";
+// foysalName[51]="mujib";       ### array array array arrayarray arrayarray array
+// console.log(foysalName);
+
+
+
+
+
+// var foysalFamily=['foyaql', 'ahmed', 'sorker', 'bamnishar'];
+// foysalFamily.push('bangladesh');
+// foysalFamily.pop();
+// foysalFamily.shift();
+// foysalFamily.unshift("baba");
+
+// console.log(foysalFamily.slice(2,3));  1,2 bad 
+// foysalFamily.splice(0, 2, "foyaql ahmed mot"); 1,2 sob
+// console.log(foysalFamily);
+
+
+
+//                             //   loops      loops          loops
+// for(var sum =1; sum <20; sum++){
+//     console.log("foysal ahmed sorker")
+// }
+
+
+
+// for(var foysal=0; foysal<=3; foysal++){
+//     console.log("my name is a foysal ahmed sorker and ")
+// }
+
+//  for(var abdul=0; abdul<=5; abdul++){
+//     console.log(abdul)
+//  }
+
+
+
+// var   foysalName=[2,5,5,2,5,6,65,55,5,565,5,];
+// for(var i =1; i<foysalName.length; i++){
+//     console.log(foysalName[i])
+// }
+
+                //    while while
+// var i=0;
+// while(i < 5){
+//     console.log("foysal first");
+//     i++
+// }
+
+
+                        // function function function
+
+
+// function fun(){
+//     var num1=10;
+//     var num2=11;
+//     var x=num1+num2     ### function basic
+//     console.log(x)
+// }
+// fun();
+// console.log("hello word");
+
+
+// function advance(num1, num2){
+//     var total=num1+num2;
+//     console.log(total);
+// }
+// advance(2,2);
+  
+
+// function advance1(num1){
+//     var total=num1*10;
+//     return total;
+// }
+// var a =advance1(10);
+// var b =advance1(20);
+// var c =advance1(30);                     function advance for my foysal ahmed sorker and 
+// var d =advance1(40);
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(a+b)
+
+
+
+            //    object object object
+// var Myinfo={Name: "foysal", FatherName: "abdul hakim sorker", MotherName:"momotaz begum", village: "bamnishair", phone:01733959986}
+// var fathername=Myinfo.FatherName;
+// console.log(Myinfo.MotherName);                  object basic object basic object basic
+// console.log(fathername);
+
+
+
+// function Myinfo(YourName, Village, Phone){
+
+//     this.YourName=YourName;
+//     this.Village=Village;
+//     this.Phone=Phone;                             advance kaj
+//     this.showme=function(){
+//         console.log(this.YourName);
+//         console.log(this.Village);
+//         console.log(this.Phone);
+//     }
+// }
+// var foysal=new Myinfo("foysal", "bamnishair", "025556");
+// var asrafulVai=new Myinfo("asraful", "dhaka", "786");
+// foysal.showme();
+// asrafulVai.showme();
+
+
+                        // ternary operator for basic to advance ok foysal
+
+// var foysal=11;
+// var myResult= (foysal>10) ? "ami tumar theke boro" : "ami tumar theke boro hobe te parlam na";
+
+// console.log(myResult);
+
+// for (i=0; i<5; i++){
+//     if(i==2){
+//         console.log("break now");    break break break break break
+//         break;
+//     }
+//     console.log(i)
+// }
+
+
+// for (i=0; i<5; i++){
+//     if(i==2){
+//         console.log("foysal tumi to two te por");
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+
+// var foysal= function fun(foyshal, foysal1){
+//     console.log(foyshal, foysal1)
+// }
+// foysal("i love javascript", "i love a python ok");
+
+
+
+// function MyFamily(YourName, FatherName, MotherName){
+//     this.YourName=YourName
+//     this.FatherName=FatherName
+//     this.MotherName=MotherName
+//   
+
+// }
+// var newOpt= new MyFamily("foysal", "abdul hakim", "momotaz begum");
+// console.log(newOpt);
+
+
+// function MyFamily(YourName, FatherName, MotherName){
+//     this.YourName=YourName
+//     this.FatherName=FatherName
+//     this.MotherName=MotherName
+//     this.SHOWME=function(){
+//         console.log(this.YourName);
+//         console.log(this.FatherName);
+//         console.log(this.MotherName);
+//     }
+
+// }
+// var newOpt= new MyFamily("foysal ahmed", "abdul hakim sorker", "momotaz begum");
+// newOpt.SHOWME();
+
+
+
+// *****************************************************************************
+                        // ternary operator for basic to advance ok foysal
+
+// var foysal=11;
+// var myResult= (foysal>10) ? "ami tumar theke boro" : "ami tumar theke boro hobe te parlam na";
+
+// console.log(myResult);
+
+// for (i=0; i<5; i++){
+//     if(i==2){
+//         console.log("break now");    break break break break break
+//         break;
+//     }
+//     console.log(i)
+// }
+
+
+// for (i=0; i<5; i++){
+//     if(i==2){
+//         console.log("foysal tumi to two te por");
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+
+// var foysal= function fun(foyshal, foysal1){
+//     console.log(foyshal, foysal1)
+// }
+// foysal("i love javascript", "i love a python ok");
+
+// var foysal=0;
+// var MyFamily=(foysal>10) ? 'ami tumar  theke bore' : 'ami tumar theke boro hote parlam na';
+// document.write(MyFamily);
+
+
+// for (i=0; i<5; i++){
+//     if(i==2){
+//         console.log("break now");
+//         break;
+//     }
+//     console.log(i)
+// }
+
+
+// for (i=0; i<5; i++){
+//     if(i==2){
+//         console.log("break now");
+//         continue
+//     }
+//     console.log(i)
+// }
+
+// for(var foysal=0; 10>foysal; foysal++){
+//     console.log("foysal ahmed sorker")
+// }
+
+
+
+
+
+// function foysal(){
+//     var num1=10;
+//     var num2=20;
+//     var total=num1+num2;
+//     console.log(total);
+// }
+// foysal();
+
+
+// function simcity(name, roll){
+//     var about=name*roll;
+//     console.log(about);
+// }
+// simcity(2,4)
+
+
+// function many(num1){
+//     var total=num1*2;
+//     return total;
+// }
+// var foysalfirst=many(10);
+// var one=many(20);
+// var two=many(30);
+// var four=many(10);
+// console.log(foysalfirst);
+// console.log(one);
+// console.log(two);
+// console.log(one+two)
+
+
+
+
+// var foysalfrd={Name:"foysal ahmed", fatherName:"abdul hakim sorker", motherName:"momotaz"}
+// console.log(foysalfrd.fatherName);
+// 1111111111111111111111111111111111111111111111111111111111111111111111111
+// function foysalbamnishair(address, name, roll){
+//     this.address=address;
+//     this.name=name;
+//     this.roll=roll;
+//     this.showme=function(){
+//         console.log(this.address)
+//         console.log(this.name)
+//         console.log(this.roll)
+//     }
+
+// }
+
+// var foysal=new foysalbamnishair("bamnishair", "foysal", 25);
+// var siam=new foysalbamnishair("mohammudpur", "siam", 20);
+// foysal.showme();
+// siam.showme();
+
+// 2222222222222222222222222222222222222222222222222222222222222222222222222222222
+// function foysalhave(home,  service, about){
+//     this.home=home;
+//     this.service=service;
+//     this.about=about;
+// }
+// var  foysalhing=new foysalhave("my first page", "my graphice designer", "i have a man");
+// console.log(foysalhing)
+// 33333333333333333333333333333333333333333333333333333333333333 sob up
+
+
+// var five =function give(name, rum){
+//     console.log(name, rum)
+// }
+// five("i love my name", "i love my rum");
+
+
+// fdjfdsklfjsdlfj  j jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+// var num1="356";
+// num1=parseFloat(num1);
+// console.log(typeof num1);
+
+// var foysalsimcity="foysal, khan, simcity, bamnishair, cumilla, bangladesh, bahrain";
+// console.log(foysalsimcity.indexOf("cumilla"))
+
+// var mot=544;
+// switch(mot){
+//     case 10:
+//         console.log("tumi 10 paiso")
+//     break;
+//     case 20:
+//         console.log("tumi 20 paiso")
+//     break;
+//     case 30:
+//         console.log("tumi 30 paiso")
+//     break;
+//     default:
+//         console.log("tumi kisu pao nai ok ")
+// }
+
+
+
+
+// var a=60;
+
+// if(a>33){
+//     console.log("d")
+// }
+// else if(a>40){
+//     console.log("c")
+// }
+// else if(a>50){
+//     console.log("b")
+// }
+// else if(a>60){
+//     console.log("a-")
+// }
+// else if(a>70){
+//     console.log("a")
+// }
+// else if(a>80){
+//     console.log("a+")
+// }
+// else{
+//     console.log("tumi dim paiso")
+// }
+
+
+// var foysalname=[1,2,3,4,5,6,7,8,9,10,11,12]
+// for(var i=0; i<foysalname.length; i++){
+//     console.log(foysalname[i])
+// }
+
+
+
+// var foysal=1;
+// var myResult=(foysal>10) ? "ami tuimar theke onek bor" :"ami tuimar theke onek bor na"
+// console.log(myResult);
+
+
+
+for(i=0; i<5; i++){
+    if(i==2){
+        console.log("brek now")
+        continue
+    }
+    console.log("foysal ahmed sorker");
+
+}
+
+
+
